7ac50c290b542063cae724f2453fa69eb9aef05a
Adam G Horn <hornadam8@gmail.com>
2020-01-27 22:09:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,2 @@
 def prime?(num)
-  if num < 2
-    false
-  else
-    matches = []
-    prime_nums = [2,3,5,7,11,13,17]
-    prime_nums.each do |prime|
-      if prime != num
-        x = num / prime
-        if x % 1 == 0
-          matches << x
-        end
-        matches = [] ? true : false
-      else
-        true
-      end
-    end
- end
- end
+  
