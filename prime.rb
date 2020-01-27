1a137b4157f3430a7d115cc2a2bed3d39002838c
Adam G Horn <hornadam8@gmail.com>
2020-01-27 22:11:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
 def prime?(num)
-  (2..(num - 1)).each do |n|
-    return false if num % n == 0
-  end
+  if num < 2
+    return false
+  else
+    (2..(num - 1)).each do |n|
+      return false if num % n == 0
+    end
   true
 end
